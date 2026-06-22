f7822062d1be672a4999b91b3c52fd66ad966809
bmartoccio <bmartoccio@itecriocuarto.org.ar>
2026-06-22 18:31:50 -0300

    Reorganiza articulos en routers y actualiza gitignore

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
-libros = [
-    { "id": 1, "nombre":"El Principito", "precio": 11000, "activo": True},
-    { "id": 2, "nombre": "Don Quijote de la Mancha", "precio": 27900, "activo": True},
-    { "id": 3, "nombre": "Cien años de Soledad", "precio": 33500, "activo": True},
-    { "id": 4, "nombre": "El señor de los Anillos", "precio": 38200, "activo": True},
-    { "id": 5, "nombre": "El diario de Ana Frank", "precio": 20000, "activo": True},
-    { "id": 6, "nombre": "Orgullo y prejuicio", "precio": 23499, "activo": True},
-    { "id": 7, "nombre": "Moby Dick", "precio": 18500, "activo": True},
-    { "id": 8, "nombre": "Rayuela", "precio": 28000, "activo": True},
-    { "id": 9, "nombre": "Cronicas de una muerte anunciada", "precio": 21000, "activo": True},
-    { "id": 10, "nombre": "Ugly Love", "precio": 22100, "activo": True},
-    { "id": 11, "nombre": "Buscando a Nemo", "precio": 12500, "activo": True},
-    { "id": 12, "nombre": "El código Da Vinci", "precio": 22500, "activo": True},
-]
+from fastapi import FastAPI, HTTPException, Body, Path, Query
+from typing import Annotated
+from pydantic import BaseModel, Field
+
+
+app = FastAPI()
+
+app.title = "LIBRERIA"
+
+app.version = "1.0"
