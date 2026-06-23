c1adb145f6b8bea4c9d6b07e747b2074bf0b9b3f
Agustin Sosa <o.sosa@itecriocuarto.org.ar>
2026-06-23 15:27:30 -0300

    agregué try/catch en método get

--- prac-js/main.js
+++ prac-js/main.js
@@ -2,7 +2,11 @@ const API_URL = "http://127.0.0.1:8000/articulos/"
 
 
 async function obtenerLibros(){
-    const res = await fetch(API_URL);
-    const datos = await res.json();
-    document.getElementById('pantalla').innerHTML = JSON.stringify(datos, null, 2);
+    try {
+        const res = await fetch(API_URL);
+        const datos = await res.json();
+        document.getElementById('pantalla').innerHTML = JSON.stringify(datos, null, 2);
+    } catch (err) {
+        console.error("Error al obtener los libros", err);
+    }
 }
